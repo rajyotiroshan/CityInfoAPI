003dc326236f089979e7ae4c226cff781dae791d
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-05-13 23:14:57 +0530

    d

---
+++ City.Api/CityDataStore.cs
@@ -0,0 +1,22 @@
+﻿using City.Api.Controllers;
+using City.Api.Model;
+namespace City.Api
+{
+    public class CityDataStore
+    {
+        public List<CityDto> Cities { 
+            get; set;
+        }
+
+        public static CityDataStore Current { get; } = new CityDataStore();
+
+        public CityDataStore()
+        {
+            Cities = new List<CityDto>(){
+                new CityDto() { Id = 1,Name="delhi", Description="chandni huouk"},
+                new CityDto() { Id = 1,Name="mumbai", Description="india gate"},
+                new CityDto() { Id = 1,Name="jaipur", Description="pink city" }
+            };
+        }
+    }
+}
